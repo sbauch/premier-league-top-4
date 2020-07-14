d1a9bcc9ad17890493e61d441093031da188f53a
Sam Bauch <sbauch@gmail.com>
2020-07-14 13:08:05 -0400

    lil design

--- src/Match.tsx
+++ src/Match.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import styles from './match.module.css';
 
 const Match = ({ dispatch, match: { id, h, a, aScore, hScore } }) => {
   const [scoreState, setScoreState] = useState({
@@ -6,11 +7,23 @@ const Match = ({ dispatch, match: { id, h, a, aScore, hScore } }) => {
     aScore,
   })
 
-  const handleScore = (field, { value }) => {
-    setScoreState({
-      ...scoreState,
-      [field]: parseInt(value, 10),
-    })
+  const increment = (field) => {
+    setScoreState(scoreState => (
+      {
+        ...scoreState,
+        [field]: scoreState[field] + 1
+      }
+    ))
+  }
+
+  const decrement = (field) => {
+
+    setScoreState(scoreState => (
+      {
+        ...scoreState,
+        [field]: scoreState[field] === 0 ? 0 : scoreState[field] - 1
+      }
+    ))
   }
 
   useEffect(() => {
@@ -18,11 +31,34 @@ const Match = ({ dispatch, match: { id, h, a, aScore, hScore } }) => {
   }, [a, dispatch, h, id, scoreState])
 
   return (
-    <div className="match">
-      <span>{h}</span>
+    <div className={styles.match}>
+      <div className={styles.teamContainer}>
+        <span>{h}</span>
+        <img src={require(`./resources/${h}.png`)} alt="team-crest" />
+      </div>
+      <div className={styles.scores}>
+        <div>
+          <div onClick={() => { increment('hScore') }} className={styles.arrowUp}></div>
+          <div onClick={() => { decrement('hScore') }} className={styles.arrowDown}></div>
+        </div>
+        <h1>
+          {scoreState.hScore}
+          &nbsp;&ndash;&nbsp;
+          {scoreState.aScore}
+        </h1>
+        <div>
+          <div onClick={() => { increment('aScore') }} className={styles.arrowUp}></div>
+          <div onClick={() => { decrement('aScore') }} className={styles.arrowDown}></div>
+        </div>
+      </div>
+      <div className={styles.teamContainer}>
+        <img src={require(`./resources/${a}.png`)} alt="team-crest" />
+        <span>{a}</span>
+      </div>
+      {/* <span>{h}</span>
       <input min="0" type='number' onChange={(e) => handleScore('hScore', e.target)} value={scoreState.hScore} />
       <input min="0" type='number' onChange={(e) => handleScore('aScore', e.target)} value={scoreState.aScore} />
-      <span>{a}</span>
+      <span>{a}</span> */}
     </div>
   )
 }
